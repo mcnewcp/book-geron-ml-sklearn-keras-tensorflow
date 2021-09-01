119df20e549bf7cf3687057761a8fa0d732a66f5
Coy McNew <mcnewcp@gmail.com>
2021-09-01 08:39:34 -0500

    first draft of data load script

---
+++ 2-end-to-end-ml-project/01-get-the-data.py
@@ -0,0 +1,30 @@
+# %%
+import os
+import tarfile
+import urllib
+
+#%%
+#some paths to use in downloading dataset
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+#function for downloading data
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+#%%
+#does the data need to be downloaded?
+download_data = False
+
+#download data if needed
+if download_data:
+    fetch_housing_data()
+
+#%%
+#load data csv
